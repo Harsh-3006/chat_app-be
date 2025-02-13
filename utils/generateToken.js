10883d9bcdf3eaf6f64d5c5e033ba5b831d7c659
Harsh saxena <harshsaxena427@gmail.com>
2025-02-13 20:24:08 +0530

    change cookie parameter

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,11 +3,19 @@ const generateTokenAndSetCookie=(userId,res)=>{
     const token=jwt.sign({userId},process.env.JWT_SECRET,{
         expiresIn:'15d'
     })
-    res.cookie("jwt",token,{
-        maxAge:15*24*60*60*1000,
-        httpOnly:true,
-        sameSite:"strict",
-    })
+    // res.cookie("jwt",token,{
+    //     maxAge:15*24*60*60*1000,
+    //     httpOnly:true,
+    //     sameSite:"None",
+    // })
+    res.cookie("jwt", token, {
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+        httpOnly: true, // Prevents client-side access to cookies
+        sameSite: "None", // Required for cross-origin cookies
+        secure: true, // Required for HTTPS (must be true in production)
+        path: "/", // Ensures the cookie is available across all routes
+    });
+    
     return token 
 }
 
